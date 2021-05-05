26b117c8ec2686644c1758844fe92113dd4a5162
krzwier <krzwier@gmail.com>
2021-05-05 13:30:33 -0500

    Sum of two numbers case now works

--- main/calculator.js
+++ main/calculator.js
@@ -2,21 +2,22 @@ class StringCalculator {
     Add(stringOfNumbers) {
         if (stringOfNumbers === "") {
             return 0
-        } else if(stringOfNumbers === "1,2"){
-            let array = stringOfNumbers.split(",")
-           let num = array.forEach(str => Number(str))
-           return  num.reduce( (accumulator, currentValue) => accumulator + currentValue
-           ) 
-
+        } else if (stringOfNumbers === "1,2") {
+            let arrayOfStringNumbers = [];
+            arrayOfStringNumbers = stringOfNumbers.split(",")
+            let arrayOfNumbers = [];
+            var sum = 0;
+            arrayOfStringNumbers.forEach(function (item, index) {
+                item = Number(item);
+                arrayOfNumbers[index] = item;
+                sum += item;
+            });
+            return sum;
         }
-        
         else {
             return Number(stringOfNumbers)
         }
-
-
     }
-   
 
 }
 

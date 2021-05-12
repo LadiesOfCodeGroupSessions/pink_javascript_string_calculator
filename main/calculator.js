dc85100dc1c25a1db2a7ff3f84f0532674b0d1f0
PaolaDM <orchidea.preziosa@gmail.com>
2021-05-12 19:31:52 +0100

    modified case customized delimiter, not passing the test

--- main/calculator.js
+++ main/calculator.js
@@ -2,9 +2,17 @@ class StringCalculator {
     Add(stringOfNumbers) {
         if (stringOfNumbers === "" ) {
             return 0
-        } else if(stringOfNumbers.substring(0,2) ==="//" ) { 
-
+        } else if(stringOfNumbers.substring(0,2) === "//" ) { 
+            let semicolon = ';';
+            let shortenString = stringOfNumbers.substring(4)
+            shortenString.split(semicolon);
+            var sum = 0;
+            shortenString.forEach(function (item) {
+                item = Number(item);
+                sum += item;
+            })
         }
+
         else {
             let arrayOfStringNumbers = [];
             arrayOfStringNumbers = stringOfNumbers.split(/[,\n]+/);

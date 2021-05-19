699cc9e75761b76fa1ac21d4cce721f9bc261afe
Charlotte Fereday <cbrfereday@gmail.com>
2021-05-19 18:31:45 +0100

    Refactors duplicated logic into helper function - tests passing!

--- main/calculator.js
+++ main/calculator.js
@@ -5,32 +5,21 @@ class StringCalculator {
         if (stringOfNumbers === "") {
             return 0
         } else if (stringOfNumbers.substring(0, 2) === "//") {
-
+            const delimiter = stringOfNumbers[2];
             let shortenString = stringOfNumbers.substring(4);
-            // let arrayFromString = [];
-            // let delimiter = shortenString[1];
-            // arrayFromString = shortenString.split(delimiter);
-            // var sum = 0;
-            // arrayFromString.forEach(function (item) {
-            //     item = Number(item);
-            //     sum += item;
-            //     console.log(sum);
-            // });
-            // return sum
-            return this.addLogic(shortenString);
+            return this.addLogic(shortenString, delimiter);
         }
 
         else {
-            return this.addLogic(stringOfNumbers);
+            const delimiter = /[,\n]+/;
+            return this.addLogic(stringOfNumbers, delimiter);
         }
-        return Number(stringOfNumbers)
     }
 
     // add an argument to this function that specifies allowable delimiters
-    addLogic(stringOfNumbers) {
-        let delimiter = stringOfNumbers[1];
+    addLogic(stringOfNumbers, allowedDelimiter) {
         let arrayOfStringNumbers = [];
-        arrayOfStringNumbers = stringOfNumbers.split(delimiter);
+        arrayOfStringNumbers = stringOfNumbers.split(allowedDelimiter);
         var sum = 0;
         arrayOfStringNumbers.forEach(function (item) {
             item = Number(item);

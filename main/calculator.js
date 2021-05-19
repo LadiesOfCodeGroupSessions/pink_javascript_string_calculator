502012600de27103ecb96ac2f9d8cf2bbb360573
krzwier <krzwier@gmail.com>
2021-05-19 12:13:09 -0500

    Refactor with helper function, one test currently failing

--- main/calculator.js
+++ main/calculator.js
@@ -1,30 +1,44 @@
 class StringCalculator {
+
+
     Add(stringOfNumbers) {
-        if (stringOfNumbers === "" ) {
+        if (stringOfNumbers === "") {
             return 0
-        } else if(stringOfNumbers.substring(0,2) === "//" ) { 
-            
+        } else if (stringOfNumbers.substring(0, 2) === "//") {
+
             let shortenString = stringOfNumbers.substring(4);
-            let arrayFromString = [];
-            let semicolon = shortenString[1];
-            arrayFromString =  shortenString.split(semicolon);
-            let sum = +arrayFromString[0] + +arrayFromString[arrayFromString.length -1];
-            console.log("this is arrayFromString ", sum);
-            return sum
+            // let arrayFromString = [];
+            // let delimiter = shortenString[1];
+            // arrayFromString = shortenString.split(delimiter);
+            // var sum = 0;
+            // arrayFromString.forEach(function (item) {
+            //     item = Number(item);
+            //     sum += item;
+            //     console.log(sum);
+            // });
+            // return sum
+            return this.addLogic(shortenString);
         }
 
         else {
-            let arrayOfStringNumbers = [];
-            arrayOfStringNumbers = stringOfNumbers.split(/[,\n]+/);
-            var sum = 0;
-            arrayOfStringNumbers.forEach(function (item) {
-                item = Number(item);
-                sum += item;
-            });
-            return sum;
+            return this.addLogic(stringOfNumbers);
         }
         return Number(stringOfNumbers)
     }
+
+    // add an argument to this function that specifies allowable delimiters
+    addLogic(stringOfNumbers) {
+        let delimiter = stringOfNumbers[1];
+        let arrayOfStringNumbers = [];
+        arrayOfStringNumbers = stringOfNumbers.split(delimiter);
+        var sum = 0;
+        arrayOfStringNumbers.forEach(function (item) {
+            item = Number(item);
+            sum += item;
+        });
+        return sum;
+    }
+
 
 }
 
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -38,8 +38,10 @@ test('input "1\n2,3" returns 6', () => {
     expect(result).toBe(6);
 })
 
-test('input "//;\n1;2" returns 3 (the delimiter is ";")', ()=>{
+
+
+test('input "//;\n1;2;4" returns 7 (the delimiter is ";")', ()=>{
     calculator = new StringCalculator(); 
-    const result = calculator.Add("//;\n1;2"); 
-    expect(result).toBe(3);
+    const result = calculator.Add("//;\n1;2;4"); 
+    expect(result).toBe(7);
 })

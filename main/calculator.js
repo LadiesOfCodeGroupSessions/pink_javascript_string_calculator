88f1aaf84a8da4c9b8813f493fb07d167fd5f40e
krzwier <krzwier@gmail.com>
2021-06-02 13:00:41 -0500

    Adds descriptive comments

--- main/calculator.js
+++ main/calculator.js
@@ -11,22 +11,35 @@ class StringCalculator {
         if (stringOfNumbers === "") {
             return 0
         } else if (stringOfNumbers.substring(0, 2) === "//") {
+            // we know that the user wants to specify a custom delimiter
             let delimiter = "";
             let shortenString = "";
             let firstIndex = stringOfNumbers.indexOf("[");
+            // here we find out if there is a '[' in the string
+            // (if so, the user wants to give us multiple custom delimiters)
             let delimiterArray = [];
-            const thirdChar = stringOfNumbers[2];
+            // empty array of delimiters
+            // ask: is there a left bracket?
             while ( firstIndex >= 0)
             {
+                // There is a left bracket!
+                // so, we need to find out where the right bracket is
                 let secondIndex = stringOfNumbers.indexOf("]")
+                // the delimiter is the text in between the left and right bracket
                 delimiter = stringOfNumbers.substring(firstIndex + 1, secondIndex)
+                // save that delimiter in the array
                 delimiterArray.push(delimiter);
+                // cut off the beginning of stringOfNumbers to eliminate // and [...]
                 stringOfNumbers = stringOfNumbers.substring(secondIndex +1);
+                // store next left bracket (if there is one)
                 firstIndex = stringOfNumbers.indexOf("[")
 
             }
+            // now we have an array of one or more custom delimiters
             console.log(delimiterArray);
-
+            // we need to transform this array into a regular expression
+            // that we can pass to addLogic()
+            const thirdChar = stringOfNumbers[2];
             if (thirdChar === "[") {
                 const index = stringOfNumbers.indexOf("]");
                 delimiter = stringOfNumbers.substring(3, index);

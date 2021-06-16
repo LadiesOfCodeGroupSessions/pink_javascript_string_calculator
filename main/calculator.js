232671f1af53f38227e8006749041176ecc43334
krzwier <krzwier@gmail.com>
2021-06-16 13:29:34 -0500

    Refactors

--- main/calculator.js
+++ main/calculator.js
@@ -3,7 +3,7 @@ class StringCalculator {
     add(stringOfNumbers) {
         const negativeNumberRegex = /-\d/gm;
         const matched = stringOfNumbers.match(negativeNumberRegex);
-        
+
         if (matched) {
             throw new Error("Negatives not allowed: " + matched)
         }
@@ -11,47 +11,14 @@ class StringCalculator {
         if (stringOfNumbers === "") {
             return 0
         } else if (stringOfNumbers.substring(0, 3) === "//[") {
-            // we know that the user wants to specify a custom delimiter
-            let delimiter = "";
-            let shortenString = "";
-            let firstIndex = stringOfNumbers.indexOf("[");
-            // here we find out if there is a '[' in the string
-            // (if so, the user wants to give us multiple custom delimiters)
-            let delimiterArray = [];
-            // empty array of delimiters
-            // ask: is there a left bracket?
-            while ( firstIndex >= 0)
-            {
-                // There is a left bracket!
-                // so, we need to find out where the right bracket is
-                let secondIndex = stringOfNumbers.indexOf("]")
-                // the delimiter is the text in between the left and right bracket
-                delimiter = stringOfNumbers.substring(firstIndex + 1, secondIndex)
-                // save that delimiter in the array
-                delimiterArray.push(delimiter);
-                // cut off the beginning of stringOfNumbers to eliminate // and [...]
-                stringOfNumbers = stringOfNumbers.substring(secondIndex +1);
-                // store next left bracket (if there is one)
-                firstIndex = stringOfNumbers.indexOf("[")
-
-            }
-            // now we have an array of one or more custom delimiters
-            //delimiter = `/[${delimiterArray.join("")}]+/`
-
-            let delimiterString = delimiterArray.join("");
-            let delimiterRegEx = `[${delimiterString}]+`;
-            let delimiterForPassing = new RegExp(delimiterRegEx);
-
-            stringOfNumbers = stringOfNumbers.substring(1);
-            // we need to transform this array into a regular expression
-            // that we can pass to addLogic()
-            return this.addLogic(stringOfNumbers, delimiterForPassing);
+            const args = this.getMultiCharacterDelimiter(stringOfNumbers);
+            return this.addLogic(args.stringOfNumbers, args.delimiterForPassing);
         } else if (stringOfNumbers.substring(0, 2) === "//") {
             // we know that the user wants to specify a custom delimiter of one character
 
             let delimiter = stringOfNumbers.charAt(2);
             stringOfNumbers = stringOfNumbers.substring(4);
-            
+
             return this.addLogic(stringOfNumbers, delimiter);
 
         }
@@ -67,12 +34,33 @@ class StringCalculator {
         var sum = 0;
         arrayOfStringNumbers.forEach(function (item) {
             item = Number(item);
-            if(item <= 1000){
+            if (item <= 1000) {
                 sum += item;
-            }    
+            }
         });
         return sum;
     }
+
+    // takes a string of numbers that begins with '//[' and processes all of the delimiter sets
+    getMultiCharacterDelimiter(stringOfNumbers) {
+        let delimiter = "";
+        let firstIndex = stringOfNumbers.indexOf("[");
+        let delimiterArray = [];
+        while (firstIndex >= 0) {
+            let secondIndex = stringOfNumbers.indexOf("]")
+            delimiter = stringOfNumbers.substring(firstIndex + 1, secondIndex)
+            delimiterArray.push(delimiter);
+            stringOfNumbers = stringOfNumbers.substring(secondIndex + 1);
+            firstIndex = stringOfNumbers.indexOf("[")
+        }
+        let delimiterString = delimiterArray.join("");
+        let delimiterRegEx = `[${delimiterString}]+`;
+        let delimiterForPassing = new RegExp(delimiterRegEx);
+        stringOfNumbers = stringOfNumbers.substring(1);
+        return { stringOfNumbers, delimiterForPassing};
+    }
 }
+
+
 
 module.exports = StringCalculator
